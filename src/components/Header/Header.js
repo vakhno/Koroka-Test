83f65951c97d2fef7dd3e7737f5af57b783cc9ce
vakhno <vakhnohere@gmail.com>
2021-02-17 18:35:08 +0200

    Added header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,49 @@
 import React from 'react'
+// styles
+import './Header.sass'
+// router-dom
+import {NavLink, Link} from 'react-router-dom'
+// content
+import LionIcon from '../../images/icons/lionIcon.svg'
+
 
 function Header() {
+	const navItems = [
+		{
+			title: "Free test",
+			path: "/test",
+		},
+		{
+			title: "Catalog",
+			path: "/catalog",
+		},
+		{
+			title: "Help",
+			path: "/help",
+		},
+	]
+
 	return (
-		<div>
-			<h1>Header</h1>
-		</div>
+		<header className='header'>
+			<div className="header__wrapper">
+				<Link className="header__logo">Logo</Link>
+				<nav className="header__nav">
+					<ul className="header__nav-items">
+						{ 
+							navItems.map( (elem, index) => { 
+								return	<li className="header__nav-item" key={`${elem.title}_${index}`}>
+											<NavLink exact to={elem.path} activeClassName="header__nav-link--active" className="header__nav-link">{elem.title}</NavLink>
+										</li>
+							})
+						}
+					</ul>
+				</nav>
+				<div className="header__profile">
+					<div className="header__profile-name">Name Namovich</div>
+					<div className="header__profile-logo"><img src={LionIcon} alt="User Icon"/></div>
+				</div>
+			</div>
+		</header>
 	)
 }
 
