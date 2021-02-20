34df33ef4b6b197b021e0c2a501fdbff5e32b4ca
vakhno <vakhnohere@gmail.com>
2021-02-20 16:49:03 +0200

    Added check marker for inputs

--- src/pages/SignIn/Form1.js
+++ src/pages/SignIn/Form1.js
@@ -20,42 +20,44 @@ function Form1({changeForm}) {
 		<form id='form1' onSubmit={handleSubmit(onSubmit)} className="profile__form" autoComplete="off">
 			<div className={`input ${errors.email ? 'input--margin-error' : ''}`}>
 				<label className={`input__label ${watchEmail ? 'input__label--active' : null}`} htmlFor="email">Email</label>
-				<div className="input__wrapper">
-				<input 
-					className={`input__area ${errors.email ? 'input--error' : (watchEmail && !errors.email) || isValid ? "input--success" : ""}`}
-					type="email" 
-					name="email" 
-					id="email"
-					ref={register({
-						required: "Required field",
-						pattern: {
-							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-							message: "invalid email address"
-						}
-					})}
-					onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
-					onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
-				/>
-				</div>
+				<label className={`${(watchEmail && !errors.email) || isValid ? "input__pseudoelement-check-mark" : ""}`}>
+					<input 
+						className={`input__area ${errors.email ? 'input--error' : (watchEmail && !errors.email) || isValid ? "input--success" : ""}`}
+						type="email" 
+						name="email" 
+						id="email"
+						ref={register({
+							required: "Required field",
+							pattern: {
+								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+								message: "invalid email address"
+							}
+						})}
+						onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
+						onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
+					/>
+				</label>
 				{errors.email ? <span className='input--error-details'>{errors.email.message}</span> : null}
 			</div>
 			<div className={`input ${errors.password ? 'input--margin-error' : ''}`}>
 				<label className={`input__label ${watchPassword ? 'input__label--active' : null}`} htmlFor="password">Password</label>
-				<input 
-					className={`input__area ${errors.password ? 'input--error' : (watchPassword && !errors.password) || isValid ? "input--success" : ""}`}
-					type="password" 
-					name="password" 
-					id="password"
-					ref={register({
-						required: "Required field",
-						minLength: {
-							value: 6,
-							message: 'Minimum 6 symbols'
-						},
-					})}
-					onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
-					onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
-				/>
+				<label className={`${(watchPassword && !errors.password) || isValid ? "input__pseudoelement-check-mark" : ""}`}>
+					<input 
+						className={`input__area ${errors.password ? 'input--error' : (watchPassword && !errors.password) || isValid ? "input--success" : ""}`}
+						type="password" 
+						name="password" 
+						id="password"
+						ref={register({
+							required: "Required field",
+							minLength: {
+								value: 6,
+								message: 'Minimum 6 symbols'
+							},
+						})}
+						onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
+						onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
+					/>
+				</label>
 				{errors.password ? <span className='input--error-details'>{errors.password.message}</span> : null}
 			</div>
 				<div className='profile__text'>Haven`t an account yet?<span className='accent-text' onClick={() => changeForm()}>&ensp;Registration</span></div>
--- src/pages/SignIn/Form2.js
+++ src/pages/SignIn/Form2.js
@@ -24,71 +24,77 @@ function Form2() {
 		<form id="form2" onSubmit={handleSubmit(onSubmit)} className="profile__form">
 			<div className={`input ${errors.name ? 'input--margin-error' : ''}`}>
 				<label className={`input__label`} htmlFor="name">Your name</label>
-				<input 
-					className={`input__area ${errors.name ? 'input--error' : (watchName && !errors.name) || isValid ? "input--success" : ""}`} 
-					type="text" 
-					name="name"
-					id="name"
-					ref={register({
-						required: "Required field",
-						minLength: {
-							value: 2,
-							message: 'Minimum 2 symbols'
-						},
-						maxLength: {
-							value: 12,
-							message: 'Maximum 12 symbols'
-						},
-					})}
-					onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
-					onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
-				/>
+				<label className={`${(watchName && !errors.name) || isValid ? "input__pseudoelement-check-mark" : ""}`}>
+					<input 
+						className={`input__area ${errors.name ? 'input--error' : (watchName && !errors.name) || isValid ? "input--success" : ""}`} 
+						type="text" 
+						name="name"
+						id="name"
+						ref={register({
+							required: "Required field",
+							minLength: {
+								value: 2,
+								message: 'Minimum 2 symbols'
+							},
+							maxLength: {
+								value: 12,
+								message: 'Maximum 12 symbols'
+							},
+						})}
+						onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
+						onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
+					/>
+				</label>
 				{errors.name ? <span className='input--error-details'>{errors.name.message}</span> : ''}
 			</div>
 			<div className={`input ${errors.email ? 'input--margin-error' : ''}`}>
 				<label className="input__label" htmlFor="email">E-mail</label>
-				<input 
-					className={`input__area ${errors.email ? 'input--error' : (watchEmail && !errors.email) || isValid ? "input--success" : ""}`} 
-					type="email" 
-					name="email" 
-					id="email"
-					ref={register({
-						required: "Required field",
-						pattern: {
-							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-							message: "invalid email address"
-						}
-					})}
-					onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
-					onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
-				/>
+				<label className={`${(watchEmail && !errors.email) || isValid ? "input__pseudoelement-check-mark" : ""}`}>
+					<input 
+						className={`input__area ${errors.email ? 'input--error' : (watchEmail && !errors.email) || isValid ? "input--success" : ""}`} 
+						type="email" 
+						name="email" 
+						id="email"
+						ref={register({
+							required: "Required field",
+							pattern: {
+								value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+								message: "invalid email address"
+							}
+						})}
+						onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
+						onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
+					/>
+				</label>
 				{errors.email ? <span className='input--error-details'>{errors.email.message}</span> : ''}
 			</div>
 			<div className={`input ${errors.phone ? 'input--margin-error' : ''}`}>
 				<label className="input__label" htmlFor="phone">Phone</label>
-				<input 
-					className={`input__area ${errors.phone ? 'input--error' : (watchPhone && !errors.phone) || isValid ? "input--success" : ""}`} 
-					type="tel" 
-					name="phone" 
-					id="phone"
-					ref={register({
-						required: "Required field",
-						minLength: {
-							value: 6,
-							message: 'Minimum 6 symbol'
-						},
-						maxLength: {
-							value: 12,
-							message: 'Maximum length is 12 symbols'
-						},
-						pattern: {
-							value: /^-?[0-9]\d*\.?\d*$/,
-							message: "Must include only numbers"
-						}
-					})}	
-					onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
-					onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
-				/>
+				<label className={`${(watchPhone && !errors.phone) || isValid ? "input__pseudoelement-check-mark" : ""}`}>
+					<input 
+						className={`input__area ${errors.phone ? 'input--error' : (watchPhone && !errors.phone) || isValid ? "input--success" : ""}`} 
+						type="tel" 
+						name="phone" 
+						id="phone"
+						ref={register({
+							required: "Required field",
+							minLength: {
+								value: 6,
+								message: 'Minimum 6 symbol'
+							},
+							maxLength: {
+								value: 12,
+								message: 'Maximum length is 12 symbols'
+							},
+							pattern: {
+								value: /^-?[0-9]\d*\.?\d*$/,
+								message: "Must include only numbers"
+							}
+						})}	
+						onFocus={(e) => e.target.labels[0].classList.add('input__label--active')}
+						onBlur={(e) => !e.target.value ? e.target.labels[0].classList.remove('input__label--active') : null}
+					/>
+				</label>
 				{errors.phone ? <span className='input--error-details'>{errors.phone.message}</span> : ''}
 			</div>
 			<label htmlFor="checkRules" className={`checkbox ${errors.checkRules ? 'checkbox--error' : ''}`}>
